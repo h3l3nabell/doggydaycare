ea4e28a147500b58a61d3f2ea293fda2b41ad905
h3l3nabell <helen.abell@chaosontoast.co.uk>
2025-03-30 19:57:59 +0100

    homepage nav buttons

--- doggy-day-care-app/src/app/(marketing)/page.tsx
+++ doggy-day-care-app/src/app/(marketing)/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -21,7 +23,14 @@ export default function Home() {
           Use DoggyDayCare to easily keep track of pets under your care. Get
           lifetime access for $299.
         </p>
-        <div className="mt-10">buttons</div>
+        <div className="mt-10 space-x-3">
+          <Button asChild>
+            <Link href="/signup">Get Started</Link>
+          </Button>
+          <Button variant="secondary" asChild>
+            <Link href="/login">Log In</Link>
+          </Button>
+        </div>
       </div>
     </main>
   );
