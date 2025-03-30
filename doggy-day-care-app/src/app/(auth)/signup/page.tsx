d49047cbb14a031b249cca7fe54288a1e20b543e
h3l3nabell <helen.abell@chaosontoast.co.uk>
2025-03-30 18:20:56 +0100

    initial setup

---
+++ doggy-day-care-app/src/app/(auth)/signup/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <main>Sign Up Page</main>;
+}
---
+++ doggy-day-care-app/src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../styles/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Doggy Daycare",
+  description: "Responsible Doggy Daycare for Awesome Dog Carers",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} text-sm text-zinc-900 bg-[#e5e8ec] antialiased min-h-screen`}
+      >
+        <h1>Doggy Day Care</h1>
+        {children}
+        <div>the best care for your best friend</div>
+      </body>
+    </html>
+  );
+}
