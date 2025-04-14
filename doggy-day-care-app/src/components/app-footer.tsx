b25869817199f6720aab281d3ac70b9961080c5a
h3l3nabell <helen.abell@chaosontoast.co.uk>
2025-04-14 18:46:44 +0100

    App Footer

--- doggy-day-care-app/src/components/app-footer.tsx
+++ doggy-day-care-app/src/components/app-footer.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function AppFooter() {
-  return <>AppFooter</>;
+  return (
+    <footer className="mt-auto border-t border-black/5 py-5 ">
+      <small className="opacity-50">
+        &copy; 2025 Chaos On Toast Limited. All rights reserved.
+      </small>
+    </footer>
+  );
 }
