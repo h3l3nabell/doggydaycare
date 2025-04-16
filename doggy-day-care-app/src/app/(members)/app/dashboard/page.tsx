048cadc4ade22bf77e7886b705ffd2702b4075fb
h3l3nabell <helen.abell@chaosontoast.co.uk>
2025-04-16 11:38:24 +0100

    refactoring

--- doggy-day-care-app/src/app/(members)/app/dashboard/page.tsx
+++ doggy-day-care-app/src/app/(members)/app/dashboard/page.tsx
@@ -1,19 +1,12 @@
+import Branding from "@/components/branding";
+import Stats from "@/components/stats";
+
 export default function Dashboard() {
   return (
     <main>
       <div className="flex items-center justify-between text-white py-8">
-        <section>
-          <h1 className="font-medium text-2xl leading-6">
-            Doggy Day<span className="font-semibold">Care</span>
-          </h1>
-          <p className="text-lg opacity-80">
-            The best care for your best friend
-          </p>
-        </section>
-        <section className="text-center">
-          <p className="text-2xl font-bold leading-6">2</p>
-          <p className="opacity-80">current guests</p>
-        </section>
+        <Branding />
+        <Stats />
       </div>
     </main>
   );
---
+++ doggy-day-care-app/src/components/branding.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import H1 from "./h1";
+
+export default function Branding() {
+  return (
+    <section>
+      <H1>
+        Doggy Day<span className="font-semibold">Care</span>
+      </H1>
+      <p className="text-lg opacity-80">The best care for your best friend</p>
+    </section>
+  );
+}
